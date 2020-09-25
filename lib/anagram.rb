a8b6507403afbd238bde5d6c820aa635f164d97c
donnymays <dtmays2@gmail.com>
2020-09-25 12:36:08 -0700

    successful fail of first test

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,41 +3,34 @@ require('pry')
 class Anagram
   attr_accessor(:word1, :word2)
   
-  def initialize
+  def initialize(word1, word2)
     @word1 = word1
     @word2 = word2
   end
 
-  def is_word?(word1, word2)
-   if !word1.split("").include?(/[aeiouy]/) || !word2.split("").include?(/[aeiouy]/)
+  
+  def are_anagrams?()
+    if @word1.scan(/[aeiouy]/).count == 0 || @word2.scan(/[aeiouy]/).count == 0
+     binding.pry
       return false
     end
+
   end
+
   
-  def are_anagrams?(word1, word2)
-   if word1.chars.sort() == word2.chars.sort()
-    return true
-   end
-  end
 
-  def case_insensitive?(word1, word2)
-    if word1.downcase().chars.sort() == word2.downcase().chars.sort()
-      return true
-    end
-  end
+  # def are_antigrams?(word1, word2)
+  #   word1_array = word1.downcase().split("")
+  #   word2_array = word2.downcase().split("")
+  #   if word1_array & word2_array = []
+  #     return true
+  #   end
+  # end
 
-  def are_antigrams?(word1, word2)
-    word1_array = word1.downcase().split("")
-    word2_array = word2.downcase().split("")
-    if word1_array & word2_array = []
-      return true
-    end
-  end
-
-  def phrase_anagrams?(phrase1, phrase2)
-    if phrase1.gsub(/[^a-zA-Z0-9\-]/,"").sort() == phrase1.gsub(/[^a-zA-Z0-9\-]/,"").sort()
-      return true
-    end
-  end
+  # def phrase_anagrams?(phrase1, phrase2)
+  #   if phrase1.gsub(/[^a-zA-Z0-9\-]/,"").sort() == phrase1.gsub(/[^a-zA-Z0-9\-]/,"").sort()
+  #     return true
+  #   end
+  # end
 
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,38 +2,11 @@ require('anagram.rb')
 require('rspec')
 
 describe(Anagram) do
-  describe("#is_word?") do
-    it("checks to see if a string is a word based on presence of vowels") do
-      anagram = Anagram.new()
-      expect(anagram.is_word?("blue", "blr")).to(eq(false))
-    end
-  end
-  
   describe("#are_anagrams?") do
-    it("checks to see if two words are anagrams") do
-      anagram = Anagram.new()
-      expect(anagram.are_anagrams?("deal", "lead")).to(eq(true))
-    end
-  end
-
-  describe("#case_insensitive?") do
-    it("checks to see if two words are anagrams regardless of lettercase") do
-      anagram = Anagram.new()
-      expect(anagram.case_insensitive?("DeAl", "lead")).to(eq(true))
-    end
-  end
-
-  describe("#are_antigrams?") do
-  it("checks to see if two words are antigrams") do
-    anagram = Anagram.new()
-    expect(anagram.are_antigrams?("fLy", "red")).to(eq(true))
-    end
-  end
-
-  describe("#phrase_antigrams?") do
-  it("checks to see if two phrases are anagrams") do
-    anagram = Anagram.new()
-    expect(anagram.are_antigrams?("blue is #neat", "blue#neat is")).to(eq(true))
+    it("checks to see if two words are words") do
+      anagram = Anagram.new("deal", "ld")
+      expect(anagram.are_anagrams?()).to(eq(true))
     end
   end
 end
+
